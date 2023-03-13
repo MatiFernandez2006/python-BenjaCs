b4d577c2434db296a89b986faa5afcb4c337282f
MatiFernandez2006 <benjamin.castro.sepulveda@alumnos.sip.cl>
2023-03-13 15:19:37 -0300

    commit

---
+++ python/fundamentals/fundamentals/file.py
@@ -0,0 +1,84 @@
+num1 = 42 #Declaracion de variables
+num2 = 2.3 #Declaracion de variables
+boolean = True
+string = 'Hello World'
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
+fruit = ('blueberry', 'strawberry', 'banana')
+print(type(fruit))
+print(pizza_toppings[1])
+pizza_toppings.append('Mushrooms')
+print(person['name'])
+person['name'] = 'George'
+person['eye_color'] = 'blue'
+print(fruit[2])
+
+if num1 > 45: #Condicional
+    print("It's greater")
+else: #Condicional
+    print("It's lower")
+
+if len(string) < 5:#Condicional
+    print("It's a short word!")
+elif len(string) > 15:#Condicional
+    print("It's a long word!")
+else:#Condicional
+    print("Just right!")
+
+for x in range(5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+while(x < 5):
+    print(x)
+    x += 1
+
+pizza_toppings.pop()
+pizza_toppings.pop(1)
+
+print(person)
+person.pop('eye_color')
+print(person)
+
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':#Condicional
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':#Condicional
+        break
+
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section
+"""
+
+# print(num3)
+# num3 = 72
+# fruit[0] = 'cranberry'
+# print(person['favorite_team'])
+# print(pizza_toppings[7])
+#   print(boolean)
+# fruit.append('raspberry')
+# fruit.pop(1)
