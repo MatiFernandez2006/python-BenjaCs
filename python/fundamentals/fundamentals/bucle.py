fb36c59334ea60103281205af491c0a75ebcf978
MatiFernandez2006 <benjamin.castro.sepulveda@alumnos.sip.cl>
2023-03-15 13:23:10 -0300

    commit3

---
+++ python/fundamentals/fundamentals/bucle.py
@@ -0,0 +1,33 @@
+#Eje 1
+for i in range(150):
+    print(i)
+
+#Eje 2
+for i in range(0,1001,5):
+    print(i)
+
+#Eje 3
+for i in range(1, 101):
+    if i % 10 == 0:
+        print("coding dojo")
+    elif i % 5 == 0:
+         print("coding")
+    else:
+        print(i)
+#Eje 4
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(sum)
+#Eje 5
+for i in range(2018,0,-4):
+    print(i)
+
+#Eje 6
+low = 2
+high = 9
+mult = 3
+
+for i in range(low,high + 1):
+    if i % mult == 0:
+        print(i)
