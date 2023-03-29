835e958a75a42006c07e5cbd0c0d4d31009ad95f
MatiFernandez2006 <benjamin.castro.sepulveda@alumnos.sip.cl>
2023-03-29 11:09:11 -0300

    jhjg

--- python/fundamentals/oop/Cuentabancaria.py
+++ python/fundamentals/oop/Cuentabancaria.py
@@ -13,7 +13,7 @@ class CuentaBancaria:
         if(self.balance - amount) >= 0:
             self.balance -= amount
         else:
-            print("Insufficient Funds: Charging a $5 fee")
+            print("Fondos insuficientes: Charging a $5 fee")
             self.balance -= 5
         return self
     
