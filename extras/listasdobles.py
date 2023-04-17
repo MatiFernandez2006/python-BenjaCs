ab672cd758ce27534866b11dc6ac82a061368b43
MatiFernandez2006 <benjamin.castro.sepulveda@alumnos.sip.cl>
2023-04-17 11:51:11 -0400

    sdfjuigdhieyr

---
+++ extras/listasdobles.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_nod
