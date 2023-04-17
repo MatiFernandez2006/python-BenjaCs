bd5b7a2d65280c9226ee4497ef4472d8a013e561
MatiFernandez2006 <benjamin.castro.sepulveda@alumnos.sip.cl>
2023-04-17 14:26:24 -0400

    simiolornosevadtucamaaaaaaa

--- extras/node_slist.py
+++ extras/node_slist.py
@@ -1,49 +1,104 @@
-class Slist:
-    def __init__(self) -> None:
-        self.head = None
 class SLNode:
-        def __init__(self) -> None:
-            self.value = Value 
-            self.next = None
+    def __init__(self, val):
+        self.value = val
+        self.next = None
 
-        def add_to_front(self, val):
-            new_node = SLNode(val)
-            current_head = self.head
-            new_node.next = current_head
-            self.head = new_node
+class SList:
+    def __init__(self):
+        self.head = None
+    def add_to_front(self, val):
+        new_node = SLNode(val)
+        current_head = self.head
+        new_node.next = current_head
+        self.head = new_node	
+        return self	
+    def print_values(self):
+        runner = self.head
+        while (runner != None):
+            print(runner.value)
+            runner = runner.next 	
+        return self
+    def add_to_back(self, val):
+        if self.head == None:	
+            self.add_to_front(val)	
+            return self	
+        new_node = SLNode(val)
+        runner = self.head
+        while (runner.next != None):
+            runner = runner.next
+        runner.next = new_node	
+        return self 
+
+    def remove_from_front(self):
+        if self.head is None:
+            return None
+        removed_value = self.head.value
+        self.head = self.head.next
+        return removed_value
+
+    def remove_from_back(self):
+        if self.head is None:
+            return None
+        if self.head.next is None:
+            removed_value = self.head.value
+            self.head = None
+            return removed_value
+        runner = self.head
+        while runner.next.next is not None:
+            runner = runner.next
+        removed_value = runner.next.value
+        runner.next = None
+        return removed_value
+
+    def remove_val(self, val):
+        if self.head is None:
+            return False
+        if self.head.value == val:
+            self.head = self.head.next
+            return True
+        runner = self.head
+        while runner.next is not None:
+            if runner.next.value == val:
+                runner.next = runner.next.next
+                return True
+            runner = runner.next
+        return False
+
+    def insert_at(self, val, n):
+        if n == 0:
+            self.add_to_front(val)
             return self
-        
-        def print_values(self):
-            current = self.head
-            while current:
-                print(current.value)
-                current =current.next 
+        new_node = SLNode(val)
+        runner = self.head
+        for _ in range(n - 1):
+            if runner is None:
+                return None
+            runner = runner.next
+        if runner is None:
+            return None
+        new_node.next = runner.next
+        runner.next = new_node
+        return self
 
-        def add_to_back(self, val):
-            if self.head == None:
-                self.add_to_front(val)
-                return self
-            new_node = SLNode(val)
-            current = self.head 
-            while (current.next !=None):
-                current = current.next
-                current.next = new_node
-                return self        
-            
-            def remove_from_front(self):
-                if self.head != None:
-                    self.head = self.head.next
-                    return self
-                
-                def remove_from_back(self):
-                    if self.head ==None:
-                        return self
-                    elif self.head.next ==None:
-                        self.head
-                        return self
-                    else:
-                           current = self.head
-                    while current.next.next !=None:
-                        current = current.next
-                        current.next= None
-                    return self
+
+
+my_list = SList()
+my_list.add_to_front(3).add_to_front(2).add_to_front(1)
+my_list.print_values() 
+
+removed_value = my_list.remove_from_front()
+print(f"Removed value: {removed_value}") 
+my_list.print_values() 
+
+removed_value = my_list.remove_from_back()
+print(f"Removed value: {removed_value}") 
+my_list.print_values() 
+
+my_list.remove_val(2)
+my_list.print_values() 
+
+my_list.add_to_front(3).add_to_front(2).add_to_front(1)
+my_list.insert_at(0, 0) 
+my_list.insert_at(4, 4) 
+my_list.insert_at(2.5, 3) 
+my_list.print_values() 
